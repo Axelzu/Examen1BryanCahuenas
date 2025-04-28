34fa57025b77d972c156b907546ce240a3be5bed
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-04-28 09:02:48 -0500

    ultimo commits

--- Bryan_CahueñasExamenP1/Models/ErrorViewModel.cs
+++ Bryan_CahueñasExamenP1/Models/ErrorViewModel.cs
@@ -1,11 +1,8 @@
-﻿using Bryan_CahueñasExamenP1.Models;
-
-namespace Bryan_CahueñasExamenP1.Models
+﻿namespace Bryan_CahueñasExamenP1.Models
 {
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
