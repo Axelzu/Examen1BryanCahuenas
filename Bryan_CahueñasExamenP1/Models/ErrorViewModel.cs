2d605ada39ecfc7e9dfdb1c6d33eb87e77804c2a
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-04-28 07:22:57 -0500

    Agregar archivos de proyecto.

---
+++ Bryan_CahueñasExamenP1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bryan_CahueñasExamenP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
