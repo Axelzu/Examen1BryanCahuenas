461eb48ff1239a1886bf82c1095bef673e80d06c
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-04-28 07:44:03 -0500

    segundo commit

--- Bryan_CahueñasExamenP1/Program.cs
+++ Bryan_CahueñasExamenP1/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Bryan_CahueñasExamenP1.Data; // ? Cambia 'TuProyecto' por el nombre real de tu proyecto
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+x
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
